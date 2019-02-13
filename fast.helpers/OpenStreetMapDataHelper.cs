15a338138c8b9272d947842b454279bacf179d52
Nick Larsen <fodylarsen@gmail.com>
2019-02-13 14:28:55 -0500

    add framework to extract OSM data

---
+++ fast.helpers/OpenStreetMapDataHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OsmSharp.Streams;
+
+namespace fast.helpers
+{
+    public static class OpenStreetMapDataHelper
+    {
+        public static IEnumerable<string> ViewAllFileData(string filename)
+        {
+            using(var fileStream = File.OpenRead(filename))
+            {
+                var source = new PBFOsmStreamSource(fileStream);
+                foreach (var element in source)
+                {
+                    yield return element.ToString();
+                }
+            }
+        }
+    }
+}
